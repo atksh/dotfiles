8e211b39dd7fa7ba39d2e27ab2876f258d08d52e
atksh <31435204+atksh@users.noreply.github.com>
2022-08-08 14:20:44 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,6 +5,7 @@ SCRIPT_DIR=$(cd $(dirname $0); pwd)
 mv ~/.vim ~/.vim.old || true
 mv ~/.vimrc ~/.vimrc.old || true
 mv ~/.gvimrc ~/.gvimrc.old || true
+mv ~/.tmux.conf ~/.tmux.conf.old || true
 
 rm -rf ~/.vim || true
 mkdir -p ~/.vim/undo
@@ -16,7 +17,8 @@ rm -rf $HOME/.dotfiles || true
 mkdir -p $HOME/.dotfiles
 ln -s $HOME/.dotfiles/.vimrc ~/.vimrc
 ln -s $HOME/.dotfiles/.gvimrc ~/.gvimrc
-cp .vimrc .gvimrc $HOME/.dotfiles
+ln -s $HOME/.dotfiles/.tmux.conf ~/.tmux.conf
+cp .vimrc .gvimrc .tmux.conf $HOME/.dotfiles
 
 # install pluings
 vim -es -u ~/.vimrc +PlugInstall +qa
